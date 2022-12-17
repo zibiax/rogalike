6519639271008cd9f0b45e181a1f4addc12e0d9f
Martin <martin.evenbom@gmail.com>
2022-12-17 10:14:31 +0100

    controls added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
 
+
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 //60 fps max
@@ -21,29 +22,50 @@ fn main(){
         .init();
 
     let mut tcod = Tcod { root };
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
     
     while !tcod.root.window_closed(){
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+
 
 }
 
 fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool{
-    false
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+    
     let key = tcod.root.wait_for_keypress(true);
     match key {
+        Key{ code: Enter,
+            alt: true,
+            ..
+        } => {    
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true,
+
+        //movement
         Key { code: Up, .. } => *player_y -= 1,
         Key { code: Down, .. } => *player_y += 1,
         Key { code: Left, .. } => *player_x -= 1,
         Key { code: Right, .. } => *player_x += 1,
 
+
+
         _ => {}
     }
-}
+    false
 
+}
