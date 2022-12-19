25287cc004354d2adcb195ba642133c7a5df3345
Martin <martin.evenbom@gmail.com>
2022-12-19 16:19:27 +0100

    start of map

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,59 @@ const SCREEN_HEIGHT: i32 = 70;
 //60 fps max
 const LIMIT_FPS: i32 = 60;
 
+const MAP_WIDTH: i32 = 90;
+const MAP_HEIGHT: i32 = 65;
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150, 
+};
+
 struct Tcod {
     root: Root,
+    con: Offscreen,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color:Color) -> Self {
+        Object { x, y, char, color }
+    }
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self. y += dy;
+    }
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
 }
 
 fn main(){
@@ -21,27 +72,47 @@ fn main(){
         .title("Rogalike Game")
         .init();
 
-    let mut tcod = Tcod { root };
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
     
+    let mut objects = [player, npc];
+
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
+
     while !tcod.root.window_closed(){
-        tcod.root.set_default_foreground(WHITE);
-        tcod.root.clear();
-        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.con.clear();
+
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
-        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+
+     //   tcod.root.wait_for_keypress(true);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0,0),
+            1.0,
+            1.0,
+    
+        );
         if exit {
             break;
+        }
+        for object in &objects {
+            object.draw(&mut tcod.con);
         }
     }
 
 
+
 }
 
-fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool{
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool{
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     
@@ -58,12 +129,10 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool{
         Key { code: Escape, .. } => return true,
 
         //movement
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
-
-
+        Key { code: Up, .. } => player.move_by (0, -1),
+        Key { code: Down, .. } => player.move_by (0, 1),
+        Key { code: Left, .. } => player.move_by (-1, 0),
+        Key { code: Right, .. } => player.move_by (1, 0),
 
         _ => {}
     }
